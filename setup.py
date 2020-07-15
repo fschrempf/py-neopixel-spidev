9179ed9738495a5a58f92c2f9a39264face29758
Frieder Schrempf <frieder.schrempf@kontron.de>
2020-07-15 11:47:47 +0200

    Add setup.py for packaging information

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-neopixel-spidev",
+    version="1.0.0",
+    author="Frieder Schrempf",
+    author_email="frieder.schrempf@kontron.de",
+    description="A module that can be used to control WS2812 addressable \
+        RGB LEDs via SPI on Linux devices using the generic Linux SPIdev driver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fschrempf/py-neopixel-spidev",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ],
+    python_requires='>=3.6',
+)
